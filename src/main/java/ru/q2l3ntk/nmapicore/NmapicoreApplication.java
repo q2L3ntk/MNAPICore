689a56f9c2a4e3d9f30021aba7a61b1aa01b5f5f
q2l3ntk <zurskij@gmail.com>
2024-11-25 21:42:08 +0300

    MNAPICore

---
+++ src/main/java/ru/q2l3ntk/nmapicore/NmapicoreApplication.java
@@ -0,0 +1,13 @@
+package ru.q2l3ntk.nmapicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmapicoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmapicoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/q2l3ntk/nmapicore/NmapicoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.q2l3ntk.nmapicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmapicoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
