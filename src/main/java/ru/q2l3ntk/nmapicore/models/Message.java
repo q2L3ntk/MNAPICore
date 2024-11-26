3d0aa9c6549c51c74745bfed2576b7e7ba017789
q2l3ntk <zurskij@gmail.com>
2024-11-26 20:06:55 +0300

    Message model fix

--- src/main/java/ru/q2l3ntk/nmapicore/models/Message.java
+++ src/main/java/ru/q2l3ntk/nmapicore/models/Message.java
@@ -2,6 +2,7 @@ package ru.q2l3ntk.nmapicore.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.Instant;
 import java.util.Date;
@@ -22,5 +23,6 @@ public class Message {
     @ManyToOne(optional = false)
     @JoinColumn(name = "conversation_id", referencedColumnName = "id")
     private Conversation conversation;
+    @DateTimeFormat
     private Date createdAt = Date.from(Instant.now());
 }
