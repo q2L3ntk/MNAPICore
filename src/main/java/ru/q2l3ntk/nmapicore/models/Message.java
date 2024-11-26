4c28af88f6f454af66502832d80bd4ed803b728b
q2l3ntk <zurskij@gmail.com>
2024-11-26 20:02:57 +0300

    Message model

---
+++ src/main/java/ru/q2l3ntk/nmapicore/models/Message.java
@@ -0,0 +1,26 @@
+package ru.q2l3ntk.nmapicore.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Data
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User sender;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "recipient_id", referencedColumnName = "id")
+    private User recipient;
+    private String body;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "conversation_id", referencedColumnName = "id")
+    private Conversation conversation;
+    private Date createdAt = Date.from(Instant.now());
+}
