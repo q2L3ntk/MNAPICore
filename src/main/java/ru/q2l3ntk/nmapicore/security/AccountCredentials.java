8a0af6545e68583714402fd2923b7fa88f09b360
q2l3ntk <zurskij@gmail.com>
2024-11-29 21:05:34 +0300

    Remake account credentials

---
+++ src/main/java/ru/q2l3ntk/nmapicore/security/AccountCredentials.java
@@ -0,0 +1,6 @@
+package ru.q2l3ntk.nmapicore.security;
+
+public class AccountCredentials {
+    public String username;
+    public String password;
+}
--- src/main/java/ru/q2l3ntk/nmapicore/security/AccountCredentials.kt
+++
@@ -1,6 +0,0 @@
-package ru.q2l3ntk.nmapicore.security
-
-class AccountCredentials {
-    lateinit var username: String
-    lateinit var password: String
-}
