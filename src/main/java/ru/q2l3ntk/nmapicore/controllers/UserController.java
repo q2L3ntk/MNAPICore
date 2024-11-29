06363e7eb626edbb4be4817278fb091da341c7e7
q2l3ntk <zurskij@gmail.com>
2024-11-29 20:55:52 +0300

    App user rest controller

---
+++ src/main/java/ru/q2l3ntk/nmapicore/controllers/UserController.java
@@ -0,0 +1,27 @@
+package ru.q2l3ntk.nmapicore.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.q2l3ntk.nmapicore.models.User;
+import ru.q2l3ntk.nmapicore.repositories.UserRepository;
+import ru.q2l3ntk.nmapicore.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+    private UserRepository userRepository;
+
+    @PostMapping("/registrations")
+    public ResponseEntity<User> create(@Validated @RequestBody User userDetails) {
+        User user = userService.attemptRegistration(userDetails);
+
+        return ResponseEntity.ok(user);
+    }
+}
