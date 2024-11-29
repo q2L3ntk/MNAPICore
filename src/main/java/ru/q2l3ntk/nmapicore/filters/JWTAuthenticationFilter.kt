fe1b413d0f747e06afb2bc14cbf984580a4e58f2
q2l3ntk <zurskij@gmail.com>
2024-11-29 13:26:14 +0300

    JWT auth filter fix

--- src/main/java/ru/q2l3ntk/nmapicore/filters/JWTAuthenticationFilter.kt
+++ src/main/java/ru/q2l3ntk/nmapicore/filters/JWTAuthenticationFilter.kt
@@ -1,12 +1,21 @@
 package ru.q2l3ntk.nmapicore.filters
 
 import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.GenericFilterBean
+import ru.q2l3ntk.nmapicore.services.tokens.TokenAuthenticationService
+import java.io.IOException
 
 class JWTAuthenticationFilter : GenericFilterBean() {
-    override fun doFilter(p0: ServletRequest?, p1: ServletResponse?, p2: FilterChain?) {
-        TODO("Not yet implemented")
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(requst: ServletRequest, response: ServletResponse, filterChain: FilterChain) {
+        val authentication = TokenAuthenticationService.getAuthentication(requst as HttpServletRequest)
+
+        SecurityContextHolder.getContext().authentication = authentication
+        filterChain.doFilter(requst, response)
     }
 }
