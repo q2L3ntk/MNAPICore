aa7d664fa3af5376defc543359f5250683f4ac19
q2l3ntk <zurskij@gmail.com>
2024-11-29 14:08:26 +0300

    App user details service

---
+++ src/main/java/ru/q2l3ntk/nmapicore/services/AppUserDetailsService.java
@@ -0,0 +1,28 @@
+package ru.q2l3ntk.nmapicore.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ru.q2l3ntk.nmapicore.repositories.UserRepository;
+
+@Component
+@AllArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            UserDetails user = (UserDetails) userRepository.findByUsername(username);
+
+            if (user == null) {
+                throw new UsernameNotFoundException(String.format("Username with the username %s not found", username));
+            } else {
+                user.getUsername();
+                user.getPassword();
+                user.getAuthorities();
+                return user;
+            }
+    }
+}
