6dd4ee965cf458bbba34db3d5bf28f245518adfb
q2l3ntk <zurskij@gmail.com>
2024-11-30 22:17:53 +0300

    filter to get access app

--- src/main/java/ru/q2l3ntk/nmapicore/config/WebSecurityConfig.java
+++ src/main/java/ru/q2l3ntk/nmapicore/config/WebSecurityConfig.java
@@ -1,34 +1,47 @@
 package ru.q2l3ntk.nmapicore.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.q2l3ntk.nmapicore.filters.JWTAuthenticationFilter;
 import ru.q2l3ntk.nmapicore.filters.JWTLoginFilter;
+import ru.q2l3ntk.nmapicore.services.AppUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class WebSecurityConfig {
-    private UserDetailsService userDetailsService;
+    private AppUserDetailsService userDetailsService;
 
-    public void configure(HttpSecurity http, AuthenticationManagerBuilder authenticationManager) throws Exception {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/users/registrations").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager.build()), UsernamePasswordAuthenticationFilter.class)
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll())
+                //.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        authenticationManager.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        return http.build();
     }
 }
