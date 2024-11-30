2c8f29b7ff4ce703a66a12b8bd00b47c0da75ea6
q2l3ntk <zurskij@gmail.com>
2024-11-30 22:15:31 +0300

    Remake signing key

--- src/main/java/ru/q2l3ntk/nmapicore/services/tokens/TokenAuthenticationService.kt
+++ src/main/java/ru/q2l3ntk/nmapicore/services/tokens/TokenAuthenticationService.kt
@@ -1,27 +1,32 @@
 package ru.q2l3ntk.nmapicore.services.tokens
 
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import java.security.Key
 import java.util.*
-import javax.crypto.SecretKey
 
 internal object TokenAuthenticationService {
     private val TOKEN_EXPIRY: Long = 864000000
-    private val SECRET: SecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512)
+    private lateinit var SECRET: String
     private val TOKEN_PREFIX = "Bearer"
     private val AUTHORIZATION_HEADER_KEY = "Authorization"
+
+    private fun getSigningKey(): Key {
+        val keyBytes: ByteArray = Decoders.BASE64.decode(this.SECRET)
+        return Keys.hmacShaKeyFor(keyBytes)
+    }
 
     fun addAuthentication(res: HttpServletResponse, username: String) {
         val JWT = Jwts.builder()
             .setSubject(username)
             .setExpiration(Date(System.currentTimeMillis() + TOKEN_EXPIRY))
-            .signWith(SECRET)
+            .signWith(getSigningKey())
             .compact()
 
         res.addHeader(AUTHORIZATION_HEADER_KEY, "$TOKEN_PREFIX $JWT")
@@ -32,7 +37,7 @@ internal object TokenAuthenticationService {
 
         if (token != null) {
             val user = Jwts.parserBuilder()
-                .setSigningKey(SECRET).build()
+                .setSigningKey(getSigningKey()).build()
                 .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
                 .body.subject
 
