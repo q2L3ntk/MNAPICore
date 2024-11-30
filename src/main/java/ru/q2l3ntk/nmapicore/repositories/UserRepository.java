ebbcb03ebef14c71b681171bc4fa8c19cc9a9109
q2l3ntk <zurskij@gmail.com>
2024-11-30 22:16:52 +0300

    Some fix

--- src/main/java/ru/q2l3ntk/nmapicore/repositories/UserRepository.java
+++ src/main/java/ru/q2l3ntk/nmapicore/repositories/UserRepository.java
@@ -3,8 +3,6 @@ package ru.q2l3ntk.nmapicore.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.q2l3ntk.nmapicore.models.User;
 
-import java.util.Optional;
-
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
     User findByPhoneNumber(String phoneNumber);
