cfd7b68b33da13036581275968f72a1fe4d40c77
q2l3ntk <zurskij@gmail.com>
2024-12-03 22:19:38 +0300

    User assembler

---
+++ src/main/java/ru/q2l3ntk/nmapicore/components/assembler/UserAssembler.java
@@ -0,0 +1,26 @@
+package ru.q2l3ntk.nmapicore.components.assembler;
+
+import org.springframework.stereotype.Component;
+import ru.q2l3ntk.nmapicore.models.User;
+import ru.q2l3ntk.nmapicore.objects.UserListVO;
+import ru.q2l3ntk.nmapicore.objects.UserVO;
+
+import java.util.List;
+
+@Component
+public class UserAssembler {
+    public UserVO toUserVO(User user) {
+        return new UserVO(
+                user.getId(),
+                user.getUsername(),
+                user.getPhoneNumber(),
+                user.getStatus(),
+                user.getCreatedAt().toString()
+        );
+    }
+
+    public UserListVO toUserListVO(List<User> users) {
+        var userVOList = users.stream().map(this::toUserVO).toList();
+        return new UserListVO(userVOList);
+    }
+}
