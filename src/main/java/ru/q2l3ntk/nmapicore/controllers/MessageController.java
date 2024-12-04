8ed0c4cb9e207687562ea2b1ac776587ba627732
q2l3ntk <zurskij@gmail.com>
2024-12-04 23:40:07 +0300

    Message controller fix

--- src/main/java/ru/q2l3ntk/nmapicore/controllers/MessageController.java
+++ src/main/java/ru/q2l3ntk/nmapicore/controllers/MessageController.java
@@ -3,6 +3,7 @@ package ru.q2l3ntk.nmapicore.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.q2l3ntk.nmapicore.components.assembler.MessageAssembler;
@@ -23,6 +24,7 @@ public class MessageController {
     private UserRepository userRepository;
     private MessageAssembler messageAssembler;
 
+    @PostMapping
     public ResponseEntity<MessageVO> create(MessageRequest messageDetails, HttpServletRequest request) {
         Principal principal = request.getUserPrincipal();
         User sender = userRepository.findByUsername(principal.getName());
