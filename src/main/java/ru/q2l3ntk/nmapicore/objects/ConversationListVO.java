ed642a59168784a293ce0158efd1c63259907ce1
q2l3ntk <zurskij@gmail.com>
2024-12-07 19:35:27 +0300

    Some fix

--- src/main/java/ru/q2l3ntk/nmapicore/objects/ConversationListVO.java
+++ src/main/java/ru/q2l3ntk/nmapicore/objects/ConversationListVO.java
@@ -1,12 +1,5 @@
 package ru.q2l3ntk.nmapicore.objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class ConversationListVO {
-    private final List<ConversationVO> conversations;
-}
+public record ConversationListVO(List<ConversationVO> conversations) { }
